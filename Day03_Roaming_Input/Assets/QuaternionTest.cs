a4c5a1a46009224defa3e36a56158442c6d98c91
Bomnamul <wdv1235@gmail.com>
2019-04-25 13:25:16 +0900

    Fix Roaming and Add Quaternion

---
+++ Day03_Roaming_Input/Assets/QuaternionTest.cs
@@ -0,0 +1,39 @@
+﻿// https://docs.unity3d.com/ScriptReference/Quaternion.html
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuaternionTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.eulerAngles = new Vector3(0f, 50f, 0f);
+        print(Mathf.Approximately(transform.eulerAngles.y, 50f));
+
+        //transform.rotation *= Quaternion.Euler(0f, 100f, 0f);         // Quaternion의 곱셈 == 현재 rotation 정보의 값에 누적시킴 (단순 곱셈 X)
+        transform.rotation = Quaternion.Euler(0f, 100f, 0f);
+        print(Mathf.Approximately(transform.eulerAngles.y, 100f));
+        transform.Rotate(Vector3.up * 90f);
+        print(Mathf.Approximately(transform.eulerAngles.y, 190f));      // Inspector에선 -170으로 표기
+        transform.rotation *= Quaternion.AngleAxis(90f, Vector3.up);
+        print(Mathf.Approximately(transform.eulerAngles.y, 280f));      // -80
+
+        transform.rotation = Quaternion.identity;                       // 단위 행렬과 비슷 (초기값)
+        print(Mathf.Approximately(transform.eulerAngles.x, 0f));
+        print(Mathf.Approximately(transform.eulerAngles.y, 0f));
+        print(Mathf.Approximately(transform.eulerAngles.z, 0f));
+        transform.rotation = Quaternion.FromToRotation(Vector3.up, transform.forward);
+        print(Mathf.Approximately(transform.eulerAngles.x, 90f));
+
+        transform.rotation = Quaternion.LookRotation(Vector3.right, Vector3.up); // forward를 right벡터로 up기준으로 돌림
+        print(Mathf.Approximately(transform.eulerAngles.y, 90f));
+        transform.rotation = Quaternion.LookRotation(Vector3.right);
+        print(Mathf.Approximately(transform.eulerAngles.y, 90f));
+
+        Quaternion q1 = Quaternion.Euler(0f, -45f, 0f);
+        Quaternion q2 = Quaternion.Euler(0f, 45f, 0f);
+        print(Mathf.Approximately(Quaternion.Angle(q1, q2), 90f));
+    }
+}
--- Day03_Roaming_Input/Assets/Roaming.cs
+++ Day03_Roaming_Input/Assets/Roaming.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections;
+﻿// https://tiborstanko.sk/lerp-vs-slerp.html
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -51,7 +53,15 @@ public class Roaming : MonoBehaviour
         Vector3 dir = nextPoint - transform.position;
         dir.y = 0;
         //transform.Rotate(Vector3.up * Vector3.Angle(transform.forward, dir) * 10f * Time.deltaTime); // Angle함수는 항상 작은 값을 선택 But 현재는 오른 방향으로만 돌기 때문에 굳이 넓은 각도로 회전
-        transform.Rotate(Vector3.up * Vector3.SignedAngle(transform.forward, dir, Vector3.up) * 10f * Time.deltaTime); // SignedAngle: Vector3.up 을 기준으로 양의 각도와 음의 각도로 나타냄 (-180 ~ 180)
-        transform.position = transform.position + dir.normalized * moveSpeed * Time.deltaTime;
+        //transform.Rotate(Vector3.up * Vector3.SignedAngle(transform.forward, dir, Vector3.up) * 10f * Time.deltaTime); // SignedAngle: Vector3.up 을 기준으로 양의 각도와 음의 각도로 나타냄 (-180 ~ 180)
+        //transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(dir, Vector3.up), 0.15f); // Vector3.up 생략 가능
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(dir), 0.15f); // Slerp: 등각 속도 운동
+
+        //transform.position = transform.position + dir.normalized * moveSpeed * Time.deltaTime;
+        //transform.Translate(dir.normalized * moveSpeed * Time.deltaTime, Space.World);
+        //transform.position = Vector3.Lerp(transform.position, nextPoint, moveSpeed * Time.deltaTime); // wikipedia lerp 방식이지만 현재의 위치 관점에서 lerp (등속 X)
+        //transform.position = Vector3.Lerp(transform.position, nextPoint, moveSpeed * Time.deltaTime / dir.magnitude); // 분모가 짧아짐 == t값이 점점 커짐 (이해 하고 넘어 갈 것)
+        //transform.position = Vector3.Slerp(transform.position, nextPoint, moveSpeed * Time.deltaTime / dir.magnitude); // 사실 Vector에 Lerp, Slerp 잘 안씀
+        transform.position = Vector3.MoveTowards(transform.position, nextPoint, moveSpeed * Time.deltaTime); // 마지막 param == 최대 거리 (maxDistanceDelta)
     }
 }

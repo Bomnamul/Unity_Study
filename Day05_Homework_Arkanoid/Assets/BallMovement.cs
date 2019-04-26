4b735d6c82b465d6fbe83ba975bee7b3bfae4ef0
Bomnamul <wdv1235@gmail.com>
2019-04-26 17:10:34 +0900

    Homework

---
+++ Day05_Homework_Arkanoid/Assets/BallMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMovement : MonoBehaviour
+{
+    private bool ready = true;
+
+    Rigidbody rb;
+    Vector3 dir = new Vector3(1, 1, 0);
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && ready == true)
+        {
+            rb.isKinematic = false;
+            rb.AddForce(dir * 1000f);
+            ready = false;
+        }
+    }
+}
---
+++ Day05_Homework_Arkanoid/Assets/PlayerMove.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    private float speed = 10f;
+
+    Rigidbody rb;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+
+        h *= speed * Time.fixedDeltaTime;
+
+        rb.MovePosition(transform.position - transform.up * h);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            transform.DetachChildren();
+        }
+    }
+}

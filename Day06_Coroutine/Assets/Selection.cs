11f2aa7ca307b02beada851f51cecb279d943e21
Bomnamul <wdv1235@gmail.com>
2019-04-29 19:41:12 +0900

    Add Homework and Prototype

---
+++ Day06_Coroutine/Assets/Selection.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Selection : MonoBehaviour
+{
+    public float angle = 90f;
+    public float duration = 1f;
+
+    bool isRotating = false;
+    float remainingAngle;
+    float remainingDuration;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && !isRotating)
+        {
+            StartCoroutine(Rotating());
+        }
+    }
+
+    IEnumerator Rotating()
+    {
+        float anglePerFrame = (remainingAngle / remainingDuration) * Time.deltaTime;
+
+        while (true)
+        {
+            if (remainingAngle < anglePerFrame)
+            {
+                anglePerFrame = remainingAngle;
+                isRotating = false;
+            }
+            transform.Rotate(Vector3.up * anglePerFrame);
+            remainingAngle -= anglePerFrame;
+            remainingDuration -= Time.deltaTime;
+            yield return null;
+        }
+    }
+}
---
+++ Day06_Homework_Jump/Assets/Jump.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : MonoBehaviour
+{
+    bool isJumping = false;
+    Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.maxAngularVelocity = 100f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && !isJumping)
+        {
+            StartCoroutine(Jumping());
+        }
+    }
+
+    IEnumerator Jumping()
+    {
+        isJumping = true;
+        print("Jump1");
+        rb.AddForce(Vector3.up * 200f);
+        yield return new WaitForSeconds(1f);
+        print("Jump2");
+        rb.AddForce(Vector3.up * 200f);
+        yield return new WaitForSeconds(1f);
+        print("Jump3");
+        rb.AddForce(Vector3.up * 500f);
+        rb.angularVelocity = Vector3.up * 30;
+        yield return new WaitForSeconds(2f);
+        isJumping = false;
+    }
+}

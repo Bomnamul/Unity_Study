cc8b10d37a09260cc8237c547309b21af1eef7fc
Bomnamul <wdv1235@gmail.com>
2019-05-02 00:20:42 +0900

    Add homework

---
+++ Day07_Homework/Assets/Healing.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Healing : MonoBehaviour
+{
+    public ParticleSystem heal;
+    
+    private void OnCollisionEnter(Collision collision)
+    {
+        Instantiate(heal, collision.transform.position, Quaternion.Euler(-90f, 0f, 0f));
+    }
+}
---
+++ Day07_Homework/Assets/Movement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float speed = 10f;
+    private float h;
+    private float v;
+    private Rigidbody rb;
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        h = Input.GetAxisRaw("Horizontal");
+        v = Input.GetAxisRaw("Vertical");
+
+        var dirH = new Vector3(h, 0f, 0f);
+        var dirV = new Vector3(0f, 0f, v);
+
+        rb.transform.Translate(dirH * speed * Time.deltaTime);
+        rb.transform.Translate(dirV * speed * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector3.up * 300f);
+        }
+    }
+}

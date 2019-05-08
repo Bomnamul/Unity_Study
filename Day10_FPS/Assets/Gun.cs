4f1957f9825fb5fc3640f0ba5d3017700a72f683
Bomnamul <wdv1235@gmail.com>
2019-05-08 13:47:06 +0900

    Add fps

--- Day10_FPS/Assets/Gun.cs
+++ Day10_FPS/Assets/Gun.cs
@@ -7,40 +7,55 @@ public class Gun : MonoBehaviour
 {
     public float fireRate = 10; // 초당 격발 횟수
     public Light muzzleFlash;
+    public GameObject shellPrefab;
+    public Transform shellEjection;
+    public GameObject impactFX;
+    public GameObject bulletHolePrefab;
 
     Camera fpsCamera;
     float nextTimeToFire = 0f;
     Vector3 originPos, smoothVel;
-    Transform fx;
-    Transform shell;
+    float recoilAngle;
+    float recoilVel;
+    //Transform fx;
+    //Transform shell;
 
     // Start is called before the first frame update
     void Start()
     {
         fpsCamera = GetComponentInParent<Camera>();
         originPos = transform.localPosition;
-        shell = transform.GetChild(1).transform;
+        //shell = transform.GetChild(1).transform;
     }
 
     // Update is called once per frame
     void Update()
     {
+        fpsCamera.transform.localRotation *= Quaternion.Euler(Vector3.left * recoilAngle);
+
         if (Input.GetButton("Fire1") && Time.time >= nextTimeToFire)
         {
             nextTimeToFire = Time.time + 1f / fireRate;
             Shoot();
         }
+
+        // kick damping
+        // SmoothDamp: 스프링처럼 돌아옴(Lerp이랑 비슷)
         transform.localPosition = Vector3.SmoothDamp(transform.localPosition,
                                                      originPos,
                                                      ref smoothVel,
                                                      0.1f);
-        // SmoothDamp: 스프링처럼 돌아옴(Lerp이랑 비슷)
+
+        // recoil damping
+        recoilAngle = Mathf.SmoothDamp(recoilAngle, 0, ref recoilVel, 0.2f);
     }
 
     private void Shoot()
     {
         muzzleFlash.enabled = true;
         Invoke("OffFlashLight", 0.05f);
+
+        MakeShell();
 
         RaycastHit hit;
         if (Physics.Raycast(fpsCamera.transform.position,
@@ -55,20 +70,44 @@ public class Gun : MonoBehaviour
                 hit.rigidbody.AddForce(fpsCamera.transform.forward * 500f);
             }
         }
+
+        GameObject fx = Instantiate(impactFX, hit.point, Quaternion.identity);
+        Destroy(fx, 0.3f);
+
+        MakeBulletHole(hit.point, hit.normal, hit.transform);
+
+        // kick
         transform.localPosition -= Vector3.forward * UnityEngine.Random.Range(0.07f, 0.3f);
 
-        StartCoroutine(ShellRoutine());
+        // recoil
+        recoilAngle += UnityEngine.Random.Range(2f, 5f);
+        recoilAngle = Mathf.Clamp(recoilAngle, 0, 25);
+
+        //StartCoroutine(ShellRoutine());
     }
 
-    IEnumerator ShellRoutine()
+    private void MakeBulletHole(Vector3 point, Vector3 normal, Transform parent)
     {
-        fx = Instantiate(shell, transform);
-        fx.GetComponent<Rigidbody>().isKinematic = false;
-        fx.GetComponent<Rigidbody>().AddForce(transform.right * 150f);
-        fx.parent = null;
-        yield return new WaitForSeconds(5f);
-        Destroy(GameObject.Find("M4_Shell(Clone)"));
+        var clone = Instantiate(bulletHolePrefab, point + normal * 0.01f, Quaternion.FromToRotation(-Vector3.forward, normal));
+        clone.transform.parent = parent;
+        Destroy(clone, 3f);
     }
+
+    private void MakeShell()
+    {
+        GameObject clone = Instantiate(shellPrefab, shellEjection);
+        clone.transform.parent = null;
+    }
+
+    //IEnumerator ShellRoutine()
+    //{
+    //    fx = Instantiate(shell, transform);
+    //    fx.GetComponent<Rigidbody>().isKinematic = false;
+    //    fx.GetComponent<Rigidbody>().AddForce(transform.right * 150f);
+    //    fx.parent = null;
+    //    yield return new WaitForSeconds(5f);
+    //    Destroy(GameObject.Find("M4_Shell(Clone)"));
+    //}
 
     void OffFlashLight()
     {

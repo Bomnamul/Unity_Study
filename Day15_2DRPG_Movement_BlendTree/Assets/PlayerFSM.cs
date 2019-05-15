3a959c5d66dc8f1119ec6f21dba4ce6ec37c612a
Bomnamul <wdv1235@gmail.com>
2019-05-15 17:11:24 +0900

    Homework done

--- Day15_2DRPG_Movement_BlendTree/Assets/PlayerFSM.cs
+++ Day15_2DRPG_Movement_BlendTree/Assets/PlayerFSM.cs
@@ -25,6 +25,31 @@ public class PlayerFSM : MonoBehaviour
         anim = GetComponent<Animator>();
     }
 
+    //IEnumerator Start()
+    //{
+    //    while(true) // yield return null로 한 프레임 단위로 반복하게 바꿔보자
+    //    {
+    //        anim.SetInteger("State", (int)state);
+    //        anim.SetInteger("PrevState", (int)prevState);
+
+    //        switch (state) // default는 안쓰는 것이 MS의 권고사항
+    //        {
+    //            case State.Idle:
+    //            case State.Walk:
+    //                yield return StartCoroutine(Move());
+    //                break;
+    //            case State.Attack:
+    //                yield return new WaitForSeconds(0.417f);
+    //                SetState(State.Idle);
+    //                break;
+    //            case State.HammerAttack:
+    //                yield return new WaitForSeconds(0.333f);
+    //                SetState(State.Idle);
+    //                break;
+    //        }
+    //    }
+    //}
+
     IEnumerator Start()
     {
         while(true)
@@ -32,56 +57,85 @@ public class PlayerFSM : MonoBehaviour
             anim.SetInteger("State", (int)state);
             anim.SetInteger("PrevState", (int)prevState);
 
-            switch (state) // default는 안쓰는 것이 MS의 권고사항
+            switch (state)
             {
                 case State.Idle:
                 case State.Walk:
-                    yield return StartCoroutine(Move());
+                    Move();
                     break;
                 case State.Attack:
                     yield return new WaitForSeconds(0.417f);
                     SetState(State.Idle);
                     break;
                 case State.HammerAttack:
-                    yield return new WaitForSeconds(0.333f);
+                    yield return new WaitForSeconds(0.583f);
                     SetState(State.Idle);
                     break;
             }
-        }
-    }
-
-    IEnumerator Move()
-    {
-        while(true)
-        {
-            float h = Input.GetAxisRaw("Horizontal");
-            float v = Input.GetAxisRaw("Vertical");
-            Vector3 heading = new Vector3(h, v, 0).normalized;
-            Vector3 movement = heading * moveSpeed * Time.deltaTime;
-            transform.position += movement;
-
-            UpdateAnimation(heading);
-
-            if (Input.GetKeyDown(KeyCode.X))
-            {
-                anim.SetTrigger("OnAttack");
-                SetState(State.Attack);
-                break;
-            }
-
-            if (Input.GetKeyDown(KeyCode.C))
-            {
-                anim.SetTrigger("OnHammerAttack");
-                SetState(State.HammerAttack);
-                break;
-            }
-
-            anim.SetInteger("State", (int)state);
-            anim.SetInteger("PrevState", (int)prevState);
-
             yield return null;
         }
     }
+
+    private void Move()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+        Vector3 heading = new Vector3(h, v, 0).normalized;
+        Vector3 movement = heading * moveSpeed * Time.deltaTime;
+        transform.position += movement;
+
+        UpdateAnimation(heading);
+
+        if (h != 0 || v != 0)
+        {
+            SetState(State.Walk);
+        }
+
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            anim.SetTrigger("OnAttack");
+            SetState(State.Attack);
+        }
+
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            anim.SetTrigger("OnHammerAttack");
+            SetState(State.HammerAttack);
+        }
+    }
+
+    //IEnumerator Move()
+    //{
+    //    while(true)
+    //    {
+    //        float h = Input.GetAxisRaw("Horizontal");
+    //        float v = Input.GetAxisRaw("Vertical");
+    //        Vector3 heading = new Vector3(h, v, 0).normalized;
+    //        Vector3 movement = heading * moveSpeed * Time.deltaTime;
+    //        transform.position += movement;
+
+    //        UpdateAnimation(heading);
+
+    //        if (Input.GetKeyDown(KeyCode.X))
+    //        {
+    //            anim.SetTrigger("OnAttack");
+    //            SetState(State.Attack);
+    //            break;
+    //        }
+
+    //        if (Input.GetKeyDown(KeyCode.C))
+    //        {
+    //            anim.SetTrigger("OnHammerAttack");
+    //            SetState(State.HammerAttack);
+    //            break;
+    //        }
+
+    //        anim.SetInteger("State", (int)state);
+    //        anim.SetInteger("PrevState", (int)prevState);
+
+    //        yield return null;
+    //    }
+    //}
 
     private void UpdateAnimation(Vector3 heading)
     {

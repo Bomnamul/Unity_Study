7af3e21a06652fa8ecedca939907e46f88d9b9eb
Bomnamul <wdv1235@gmail.com>
2019-05-31 15:53:58 +0900

    Add NavMeshAgent

---
+++ Day17_TPS/Assets/MobAwake.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MobAwake : StateMachineBehaviour
+{
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        Debug.Log("Awake Enter");
+        Debug.Log(stateInfo.IsName("Base Layer.Awake"));
+        Debug.Log(animator.GetCurrentAnimatorStateInfo(0).IsName("Base Layer.Idle"));
+        Debug.Log(animator.GetCurrentAnimatorStateInfo(0).fullPathHash);
+        Debug.Log(animator.GetNextAnimatorStateInfo(0).IsName("Base Layer.Awake"));
+        Debug.Log(animator.GetNextAnimatorStateInfo(0).fullPathHash);
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        Debug.Log("Awake Update");
+        Debug.Log(stateInfo.IsName("Base Layer.Awake"));
+        Debug.Log(animator.GetCurrentAnimatorStateInfo(0).fullPathHash);
+        Debug.Log(animator.GetNextAnimatorStateInfo(0).fullPathHash);
+    }
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        Debug.Log("Awake Exit");
+        Debug.Log(stateInfo.IsName("Base Layer.Awake"));
+        Debug.Log(animator.GetCurrentAnimatorStateInfo(0).IsName("Base Layer.Chase"));
+        Debug.Log(animator.GetCurrentAnimatorStateInfo(0).fullPathHash);
+        Debug.Log(animator.GetNextAnimatorStateInfo(0).IsName(""));
+        Debug.Log(animator.GetNextAnimatorStateInfo(0).fullPathHash);
+    }
+
+    // OnStateMove is called right after Animator.OnAnimatorMove()
+    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that processes and affects root motion
+    //}
+
+    // OnStateIK is called right after Animator.OnAnimatorIK()
+    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that sets up animation IK (inverse kinematics)
+    //}
+}
+
+/*
+ * Enter
+ * 
+ * 
+ * Update
+ * 
+ * 
+ * Exit
+ * 
+ * 
+ * 
+ * Idle: 1432961145
+ * Awake: 584177647
+ * Chase: -764388577
+ * Empty: 0
+ * 
+ * 구분하여 정리
+ */
---
+++ Day17_TPS/Assets/MobChaseNavMesh.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MobChaseNavMesh : StateMachineBehaviour
+{
+    public float chaseSpeed = 2f;
+
+    MobController mc;
+    NavMeshAgent nmAgent;
+
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        mc = animator.GetComponent<MobController>();
+        nmAgent = animator.GetComponent<NavMeshAgent>();
+        nmAgent.speed = chaseSpeed;
+        nmAgent.isStopped = false;
+        mc.GetComponent<Rigidbody>().isKinematic = true;
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        Transform target = animator.GetComponent<MobController>().player;
+        Vector3 dir = target.position - animator.transform.position;
+        dir.y = 0;
+        nmAgent.destination = target.position;
+    }
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if (!animator.GetCurrentAnimatorStateInfo(0).IsName("Base Layer.Combat.MeleeChase1")
+            && !animator.GetCurrentAnimatorStateInfo(0).IsName("Base Layer.Combat.MeleeChase"))
+        {
+            nmAgent.isStopped = true;
+            mc.GetComponent<Rigidbody>().isKinematic = false;
+        }
+    }
+
+    // OnStateMove is called right after Animator.OnAnimatorMove()
+    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that processes and affects root motion
+    //}
+
+    // OnStateIK is called right after Animator.OnAnimatorIK()
+    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that sets up animation IK (inverse kinematics)
+    //}
+}

1cec5b516dcd818d30cba8a7d55c71a597d81b33
Bomnamul <wdv1235@gmail.com>
2019-06-03 12:28:12 +0900

    Add UnityTips(Attribute, Pooling, Singleton)

---
+++ Day29_UnityTips_Attribute_Singleton_Pooling/Assets/Player.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print(MobManager.Instance.myGlobalVar == "whatever"); // MobManager의 static한 변수 Instance (하나만 존재)
+        print(MobManager.Instance.MobCount() == 100);
+    }
+}
---
+++ Day29_UnityTips_Attribute_Singleton_Pooling/Assets/Shell.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shell : MonoBehaviour
+{
+    private void OnEnable() // 초기화 작업 (ex: rigidbody에서 velocity 값을 초기화 시켜줘야 함)
+    {
+        print("OnEnable");
+    }
+
+    private void OnDisable() // 후처리 작업
+    {
+        print("OnDisable");
+    }
+}

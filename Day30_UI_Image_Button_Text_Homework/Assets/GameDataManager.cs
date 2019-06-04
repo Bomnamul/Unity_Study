1f5318d4adcd67fea5137ef30b7f4ea1592f680e
Bomnamul <wdv1235@gmail.com>
2019-06-04 21:19:14 +0900

    Add RightHealthBar and Timer(Homework)

---
+++ Day30_UI_Image_Button_Text_Homework/Assets/GameDataManager.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameDataManager : Singleton<GameDataManager>
+{
+    protected GameDataManager() { }
+
+    float currentHealth = 100f;
+    float maxHealth = 100f;
+    int timeCount = 99;
+
+    int timeStamp = 0;
+
+    public void TakeDamage(float amount)
+    {
+        currentHealth -= amount;
+        if (currentHealth <= 0f)
+        {
+            currentHealth = 0f;
+        }
+        UpdateTimeStamp();
+    }
+
+    public void Heal(float amount)
+    {
+        currentHealth += amount;
+        currentHealth = Mathf.Clamp(currentHealth, 0f, maxHealth);
+        UpdateTimeStamp();
+    }
+
+    public float GetCurrentHealth()
+    {
+        return currentHealth;
+    }
+
+    public float GetMaxHealth()
+    {
+        return maxHealth;
+    }
+
+    public int GetTimeCount()
+    {
+        return timeCount;
+    }
+    
+    public void SetTimeCount(int t)
+    {
+        timeCount = t;
+        UpdateTimeStamp();
+    }
+
+    void UpdateTimeStamp() // Dirty bit?
+    {
+        timeStamp++;
+        if (timeStamp <= 0) // Prevent overflow
+        {
+            timeStamp = 1;
+        }
+    }
+
+    public int GetTimeStamp()
+    {
+        return timeStamp;
+    }
+}
---
+++ Day30_UI_Image_Button_Text_Homework/Assets/UIController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour
+{
+    public Health leftSide;
+    public Health rightSide;
+    public Button healButton;
+    public Button damageButton;
+    public Text timeText;
+
+    int uiTimeStamp = 0;
+    int currentTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        damageButton.onClick.AddListener(() => GameDataManager.Instance.TakeDamage(10f));
+        healButton.onClick.AddListener(() => GameDataManager.Instance.Heal(20f));
+        currentTime = GameDataManager.Instance.GetTimeCount();
+        StartCoroutine(TimeCounter());
+    }
+
+    IEnumerator TimeCounter()
+    {
+        while (currentTime > 0)
+        {
+            yield return new WaitForSeconds(1f);
+            GameDataManager.Instance.SetTimeCount(currentTime--);
+        }
+    }
+
+    // Update is called once per frame
+    void Update() // MVC (Model View Controller)
+    {
+        int timeStamp = GameDataManager.Instance.GetTimeStamp();
+        if (timeStamp != uiTimeStamp) // Polling
+        {
+            uiTimeStamp = timeStamp;
+            float currentHealth = GameDataManager.Instance.GetCurrentHealth();
+            float maxHealth = GameDataManager.Instance.GetMaxHealth();
+            timeText.text = currentTime.ToString();
+            leftSide.UpdateHealthBar(currentHealth, maxHealth);
+            rightSide.UpdateHealthBar(currentHealth, maxHealth);
+        }
+    }
+}

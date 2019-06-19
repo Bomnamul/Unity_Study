b79dad92e37249aabf709fca74c89f3f0d42107f
Bomnamul <wdv1235@gmail.com>
2019-06-19 12:23:57 +0900

    Add HTTP and Json

---
+++ Day36_HTTP_Json/Assets/HandlingJson.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class HandlingJson : MonoBehaviour
+{
+    string pathJson;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pathJson = Application.persistentDataPath + "/gameScore.json";
+        print(pathJson);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            SaveJsonFile();
+        }
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            LoadJsonFile();
+        }
+    }
+
+    private void LoadJsonFile()
+    {
+        if (File.Exists(pathJson))
+        {
+            string dataAsJson = File.ReadAllText(pathJson);
+            GameScore newGS = JsonUtility.FromJson<GameScore>(dataAsJson);
+            print(newGS);
+        }
+        else
+        {
+            print("No file !");
+        }
+    }
+
+    private void SaveJsonFile()
+    {
+        GameScore gs = new GameScore();
+        gs.level = 10;
+        gs.timeElapsed = 300;
+        gs.playerName = "dga";
+        gs.dontCare = "Don't care";
+
+        Item item = new Item();
+        item.itemId = 1000;
+        item.iconImage = "image1.png";
+        item.price = 10000;
+        gs.items = new List<Item>();
+        gs.items.Add(item);
+        Item item1 = new Item();
+        item1.itemId = 2000;
+        item1.iconImage = "image2.png";
+        item1.price = 20000;
+        gs.items.Add(item1);
+
+        string dataAsJson = JsonUtility.ToJson(gs, true);   // JsonUtility는 배열을 인식하지 못함 (Wrapping)
+        print(dataAsJson);
+        File.WriteAllText(pathJson, dataAsJson);    // Sync(Block), Async version이 따로 있을 것
+    }
+}
+
+[Serializable]    // Json 자체적으로는 어떻게 Serialize 할지 모르기에 명시해야함, Map은 Serialize 안됨 (list type으로 바꿔쓰던지 해야함)
+public class GameScore
+{
+    public int level;
+    public float timeElapsed;
+    public string playerName;
+
+    [NonSerialized]    // 제외
+    public string dontCare;
+
+    public List<Item> items;
+
+    public override string ToString()
+    {
+        return level + ", " + timeElapsed + ", " + playerName + ", " + items.Count;
+    }
+}
+
+[Serializable]
+public class Item
+{
+    public int itemId;
+    public string iconImage;
+    public int price;
+}

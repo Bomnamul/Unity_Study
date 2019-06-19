41eec121444c410018d5462e6e5afe662ff974b9
Bomnamul <wdv1235@gmail.com>
2019-06-19 17:38:29 +0900

    Add Homework

---
+++ Day36_HTTP_Json_Homework/Assets/DownloadTexture.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class DownloadTexture : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(GetTextureFromURL());
+    }
+
+    IEnumerator GetTextureFromURL()
+    {
+        string url = "https://avatars1.githubusercontent.com/u/43165018?s=460&v=4";
+
+        // 1.
+        using (UnityWebRequest uwr = UnityWebRequestTexture.GetTexture(url))    // 알아서 반납
+        {
+            yield return uwr.SendWebRequest();
+            if (uwr.isNetworkError || uwr.isHttpError)
+            {
+                print(uwr.error);
+            }
+            else
+            {
+                var texture = DownloadHandlerTexture.GetContent(uwr);
+                GetComponent<Renderer>().material.mainTexture = texture;
+            }
+        }
+
+        // 2.
+        //{
+        //    UnityWebRequest uwr = UnityWebRequestTexture.GetTexture(url);
+        //    try
+        //    {
+        //        yield return uwr.SendWebRequest();
+        //        if (uwr.isNetworkError || uwr.isHttpError)
+        //        {
+        //            print(uwr.error);
+        //        }
+        //        else
+        //        {
+        //            var texture = DownloadHandlerTexture.GetContent(uwr);
+        //            GetComponent<Renderer>().material.mainTexture = texture;
+        //        }
+        //    }
+        //    finally
+        //    {
+        //        if (uwr != null)
+        //            uwr.Dispose();  // 메모리에서 제거 요청 (OS측면에서 중요한 자원이기에 Dispose로 항상 제거해야함)
+        //    }
+        //}
+
+        // 3.
+        //using (WWW www = new WWW(url))
+        //{
+        //    yield return www;
+        //    GetComponent<Renderer>().material.mainTexture = www.texture;
+        //}
+    }
+}

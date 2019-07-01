a85143a7d880200f8f017407b61d6aafc5823a07
Bomnamul <wdv1235@gmail.com>
2019-07-02 01:46:00 +0900

    Add Drop Potion

--- Day40_ItemSlot/Assets/Slot.cs
+++ Day40_ItemSlot/Assets/Slot.cs
@@ -1,8 +1,29 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Slot : MonoBehaviour
 {
     public int slotId;
+    public Button xButton;
+    public GameObject healthPotion;
+
+    private void Start()
+    {
+        xButton.onClick.AddListener(ButtonClicked);
+    }
+
+    private void ButtonClicked()
+    {
+        print("Drop Potion: " + slotId);
+        var player = GameFlow.instance.player;
+        if (transform.childCount != 0)
+        {
+            print("Entered");
+            Instantiate(healthPotion, player.transform.localPosition + Vector3.right, Quaternion.identity);
+            Destroy(transform.GetChild(0).gameObject);
+        }
+    }
 }

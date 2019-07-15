15cf6c9fdc1a4917909a308b56343f92dba923bb
Bomnamul <wdv1235@gmail.com>
2019-07-15 13:45:05 +0900

    Add Tilemap and AStar Algorithm

---
+++ Day47_2DRPG_Timeline/Assets/Scripts/Readme.cs
@@ -0,0 +1,13 @@
+﻿/*
+ * Timeline 시 주의 사항
+ * 
+ *  1. Animation timeline track에 들어간 객체는 Animator가 자동으로 생성이 된다. Animator controller가 없어도 제거하면 안된다.
+ * 
+ *  2. Animation용으로 사용되는 Animator랑 중복으로 사용할 수 없다. 하위 Model로 분리해야 한다.
+ * 
+ *  3. 실시간 생성되는(Instantiate) 객체는 실행 시점에 Binding 해야 한다.
+ * 
+ *  4. 실시간에 생성되는 객체에 대한 Timeline은 Local transform 기준으로 Animation 작업을 해라. World zero로 옮겨서 작업해라.
+ * 
+ *  5. Track offset 값을 Clear 해라. Track/Clip 두 개가 있다. 모두 Clear.
+ */
---
+++ Day47_2DRPG_Timeline/Assets/Slime.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class Slime : MonoBehaviour
+{
+    AIDestinationSetter destSetter;
+    Transform target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        destSetter = GetComponent<AIDestinationSetter>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (destSetter.target == null)
+        {
+            destSetter.target = GameObject.FindGameObjectWithTag("Player")?.transform;
+        }
+    }
+}

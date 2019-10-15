47b9576aec253914189f52aa2679f2203fdbeeb6
Bomnamul <wdv1235@gmail.com>
2019-10-15 14:48:52 +0900

    Add Addressable

---
+++ Day83_UnityAddressables/Assets/LoadCube.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+public class LoadCube : MonoBehaviour
+{
+    //public GameObject cubePrefab;
+    public AssetReference cubePrefab;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //Instantiate(cubePrefab);
+            StartCoroutine(LoadCubePrefab());
+        }
+    }
+
+    IEnumerator LoadCubePrefab()
+    {
+        yield return cubePrefab.InstantiateAsync(Vector3.zero, Quaternion.identity);
+    }
+}
---
+++ Day83_UnityAddressables/Assets/Scripts/SkyboxManager.cs
@@ -0,0 +1,32 @@
+﻿//http://thegamedev.guru/unity-addressables/tutorial-learn-the-basics/
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+
+public class SkyboxManager : MonoBehaviour
+{
+    [SerializeField] private List<AssetReference> _skyboxMaterials;
+    AsyncOperationHandle _currentHandle;
+
+    public void SetSkybox(int skyboxIndex)
+    {
+        //RenderSettings.skybox = _skyboxMaterials[skyboxIndex];
+        StartCoroutine(SetSkyboxInternal(skyboxIndex));
+    }
+
+    IEnumerator SetSkyboxInternal(int skyboxIndex)
+    {
+        if (_currentHandle.IsValid())
+        {
+            Addressables.Release(_currentHandle);
+        }
+        AssetReference skyboxMaterialReference = _skyboxMaterials[skyboxIndex];
+        _currentHandle = skyboxMaterialReference.LoadAssetAsync<Material>();
+        yield return _currentHandle;
+        RenderSettings.skybox = (Material)_currentHandle.Result;
+    }
+}
